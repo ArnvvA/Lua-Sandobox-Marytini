0a0f8e62f4d8d600bd0950649ece1c7f60465963
Saransh <144621066+SaranshSondhi@users.noreply.github.com>
2025-04-22 21:34:09 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -7,71 +7,90 @@ class base:
         self.salty = a2
         self.sour = a3
         self.bitter = a4
-    def overdone(self):
+    """def overdone(self):
         if self.sweet > 10 or self.salty > 10 or self.sour > 10 or self.bitter > 10:
-            return True
+            return True"""
     def match(self, target):
-        if (target["sweet"] - 1 <= self.sweet <= target["sweet"] + 1)\
-            and (target["salty"] - 1 <= self.salty <= target["salty"] + 1)\
-            and (target["sour"] - 1 <= self.sour <= target["sour"] + 1)\
-            and (target["bitter"] - 1 <= self.bitter <= target["bitter"] + 1):
+        if (target["sweet"] == self.sweet)\
+            and (target["salty"] == self.salty)\
+            and (target["sour"] == self.sour)\
+            and (target["bitter"] == self.bitter):
             return True
         else:
             return False
+    def score_calc(self, mix):
+        score = 100
+        parameters = ["sweet","salty","sour","bitter"]
+        for i in range(4):
+            score -= (abs(mix[parameters[i]] - self.__dict__[parameters[i]]))*25
+        return score
 
 
-#Target Drink
-targ = base("target",randint(0,10),randint(0,10),randint(0,10),randint(0,10))
-target = targ.__dict__
-#Descriptions
-for i in ["sweet", "salty","sour","bitter"]:
-    if target[i] <= 1:
-        print("not", end = " ")
-    elif target[i] <= 5:
-        print("slightly", end = " ")
-    elif target[i] <= 8:
-        print("moderately", end = " ")
-    elif target[i] < 11:
-        print("Extremely",  end = " ")
-    print(i)
-
-#Additions
 """
+#Additions
 sugar = base("sugar",2,0,0,0)
 salt = base("salt",0,2,0,0)
 coffee = base("coffee",0,0,0,3)
 lemon = base("lemon",0,0,2,0)
 citrus = base("citrus",0,0,4,1)
 """
-#Start Mixing!!!
-starter = base("mix",0,0,0,0)
 #Setting marks
-choice = input("Enter Choice of base (L/D/S)    :")
+"""choice = input("Enter Choice of base (L/D/S)    :")
 if choice == 'D':
-    starter.bitter += 4
+    starter.bitter += 1
     starter.sweet += 1
 elif choice == 'L':
     starter.bitter += 6
 elif choice == 'S':
-    starter.bitter += 1
+    starter.bitter += 1"""
+
+
+#Target Drink
+def definetarg():
+    targ = base("target",randint(0,2),randint(0,2),randint(0,2),randint(0,2))
+    target = targ.__dict__
+    print(target)
+    return target
+
+target = definetarg()
+stage, multiplier = 1, 1
+i = 0
 while True:
-    #ingredient adding
-    ing = int(input("Choose ingredient to add (Sugar, Salt, Coffee, Lemon, Citrus):     "))
-    if ing == 0:
-        starter.sweet += 2
-    elif ing == 1:
-        starter.salty += 2
-    elif ing == 2:
-        starter.bitter += 3
-    elif ing == 3:
-        starter.sour += 2
-    elif ing == 4:
-        starter.sour += 4
-        starter.bitter += 1
-    print(starter.__dict__.values())
-    if starter.overdone():
-        print("EWWWWW")
-        break
+
+    print("Stage", stage, "Round", i+1)
+    score = 0
+
+    #Min score for each round
+    min_score = 50*stage
+    print("Minimum Score for this round is", min_score)
+
+    #Start Mixing
+    starter = base("mix",0,0,0,0)
+    choice = list(map(int,input("Select Tier of each ingredient (0/1/2)    :").split()))
+    starter.sweet += choice[0]
+    starter.salty += choice[1]
+    starter.sour += choice[2]
+    starter.bitter += choice[3]
+
+    score += starter.score_calc(target)
+    score *= multiplier
+    print("Your score is", score, " was multiplied by", multiplier)
+    
     if starter.match(target):
-        print("CONGRATULATIONS")
+        score = multiplier*100
+        multiplier += 4-i
+        print("Guessed Correctly")
+        target = definetarg()
+        i, stage = 0, stage+1
+    
+    if score >= min_score:
+        multiplier -= 1
+        target = definetarg()
+        i , stage = 0, stage+1
+        print("Close one that")
+    elif i == 3:
+        print("Game Over, made it to", stage)
         break
+    else:
+        print("Try again")
+        i+=1
